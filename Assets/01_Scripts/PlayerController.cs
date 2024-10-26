310ad3fc3945c73a41a11ec253e2cac7bd68dd27
MaIcer <maicer@vanilage.kr>
2024-10-26 10:04:01 +0900

    Revert "호헹헹"
    This reverts commit 39bd02aa7938c47a48665b2e028cf8e28b922331.

--- Assets/01_Scripts/PlayerController.cs
+++ Assets/01_Scripts/PlayerController.cs
@@ -4,110 +4,100 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField]
-    private float moveSpeed = 5f;         // 플레이어 이동 속도
-    [SerializeField]
-    private float jumpForce = 10f;        // 플레이어 점프 힘
-    [SerializeField]
-    private LayerMask groundLayer;        // Ground 레이어를 선택할 수 있도록 추가
-    private bool isGrounded;                               // 플레이어가 바닥에 닿았는지 확인하는 변수
-    private float horizontal;                              // 좌우 입력 감지를 위한 변수
-    private Rigidbody2D rb2D;                              // 2D 물리 처리를 위한 리지드바디
-    private Animator animator;                             // 애니메이션을 관리하는 컴포넌트
-    private float groundCheckRadius = 0.2f;                // 바닥 감지를 위한 원의 반지름
-    private Transform groundCheck;                         // 캐릭터 아래에 있는 바닥 감지 위치
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float jumpForce;
+    private bool isGrounded;
+    private bool isMove;
+    float horizontal;
+    Transform transform;
+    Rigidbody2D rigidbody2D;
+    private Animator animator;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        rb2D = GetComponent<Rigidbody2D>();                // Rigidbody2D 컴포넌트 가져오기
-        animator = GetComponent<Animator>();               // Animator 컴포넌트 가져오기
-        groundCheck = transform.Find("GroundCheck");       // GroundCheck라는 이름의 하위 오브젝트 가져오기 (바닥 감지용)
+       rigidbody2D = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        transform = GetComponent<Transform>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        horizontal = Input.GetAxisRaw("Horizontal");       // 좌우 입력 감지 (수평 입력)
-        Jump();                                            // 점프 함수 호출
-        UpdateAnimation();                                 // 애니메이션 상태 업데이트
-        Reverse();                                         // 플레이어 방향 반전
+        Jump();
+        UpdateAnimation();
+        Reverse();
     }
 
-    // FixedUpdate is called at a fixed time interval (used for physics updates)
-    void FixedUpdate()
+    private void FixedUpdate()
     {
-        Move();                                            // 플레이어 이동 처리
-        CheckGrounded();                                   // 바닥에 닿았는지 확인
+        Move();
     }
 
-    // 플레이어의 이동을 처리하는 함수
     void Move()
     {
-        rb2D.velocity = new Vector2(horizontal * moveSpeed, rb2D.velocity.y); // 좌우 이동 처리
-    }
 
-    // 점프 처리를 위한 함수
+        horizontal = Input.GetAxisRaw("Horizontal");
+
+        //print(horizontal);
+        
+        rigidbody2D.velocity = new Vector2(horizontal * moveSpeed*Time.deltaTime, rigidbody2D.velocity.y);
+        
+    }
     void Jump()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)  // 스페이스바를 누르고 땅에 있을 때만 점프
+        if (  Input.GetKeyDown(KeyCode.Space)  && isGrounded )
         {
-            rb2D.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);  // 위쪽으로 힘을 주어 점프
+            rigidbody2D.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
         }
+           
     }
-
-    // 애니메이션 상태 업데이트 함수
     void UpdateAnimation()
     {
-        animator.SetBool("isMove", horizontal != 0);       // 좌우 이동 시 이동 애니메이션 적용
-        animator.SetBool("isJump", !isGrounded);           // 점프 중일 때 점프 애니메이션 적용
-    }
+        if(horizontal != 0)
+        {
+            animator.SetBool("isMove", true);
+        }
+        else
+        {
+            animator.SetBool("isMove", false);
 
-    // 캐릭터 방향을 좌우로 반전시키는 함수
+        }
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            animator.SetBool("isJump", true);
+        }
+        else
+        {
+            animator.SetBool("isJump", false);
+        }
+    }
     void Reverse()
     {
-        if (horizontal < 0)                                // 왼쪽으로 이동 중일 때
+        if (horizontal == -1)
         {
-            transform.localScale = new Vector3(-7, 7, 1);  // 캐릭터를 왼쪽으로 반전
+            transform.localScale = new Vector3(-7, 7, 1);
         }
-        else if (horizontal > 0)                           // 오른쪽으로 이동 중일 때
+        else if (horizontal == 1)
         {
-            transform.localScale = new Vector3(7, 7, 1);   // 캐릭터를 오른쪽으로 반전
+            transform.localScale = new Vector3(7, 7, 1);
+
         }
     }
-
-    // 바닥에 닿았는지 확인하는 함수 (Raycast 또는 OverlapCircle 사용)
-    void CheckGrounded()
-    {
-        // GroundCheck 위치에서 원형 범위로 바닥에 닿았는지 체크
-        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
-    }
-
-    // 충돌이 시작될 때 호출되는 함수 (Ground와의 충돌만 처리)
     void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.CompareTag("Ground"))     // 충돌한 오브젝트가 "Ground" 태그를 가질 때만 처리
+        if (collision.gameObject.CompareTag("Ground")) // 충돌한 오브젝트가 "Ground" 태그를 가진 경우
         {
-            isGrounded = true;                             // 바닥에 닿았다고 설정
+            isGrounded = true; // 땅에 붙어 있는 상태로 설정
         }
     }
 
-    // 충돌이 끝날 때 호출되는 함수 (Ground와의 충돌만 처리)
     void OnCollisionExit2D(Collision2D collision)
     {
-        if (collision.gameObject.CompareTag("Ground"))     // 바닥에서 벗어났을 때 처리
+        if (collision.gameObject.CompareTag("Ground")) // 충돌한 오브젝트가 "Ground" 태그를 가진 경우
         {
-            isGrounded = false;                            // 바닥에서 떨어졌다고 설정
-        }
-    }
-
-    // 바닥 감지 범위를 유니티 에디터에서 시각적으로 확인하기 위한 함수
-    private void OnDrawGizmos()
-    {
-        if (groundCheck != null)
-        {
-            Gizmos.color = Color.red;                      // Gizmo 색상을 빨간색으로 설정
-            Gizmos.DrawWireSphere(groundCheck.position, groundCheckRadius); // GroundCheck 위치에 원형 범위를 그려줌
+            isGrounded = false; // 땅에서 벗어난 상태로 설정
         }
     }
 }
--- Assets/01_Scripts/SpongeController.cs
+++ Assets/01_Scripts/SpongeController.cs
@@ -10,7 +10,7 @@ public enum Direction
     North,
     South,
     None
-}
+} 
 
 public class SpongeController : MonoBehaviour
 {
@@ -18,93 +18,90 @@ public class SpongeController : MonoBehaviour
     float shootingSpeed;
     public Direction shotDirection = Direction.None;
 
-    [SerializeField]
-    GameObject player;
+    [SerializeField] GameObject player;
 
-    [SerializeField]
-    bool isTouchedPlayer = false;
+    [SerializeField] bool isTouchedPlayer = false;
     private bool Sponge;
     Rigidbody2D playerRigidbody2D;
     SpriteRenderer color;
-    [SerializeField]
-    bool isPlayerInSponge = false;
+    [SerializeField] bool isPlayerInSponge = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        color = GetComponent<SpriteRenderer>();
+        color = GetComponent<SpriteRenderer>(); 
         playerRigidbody2D = player.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
         Absorption();
     }
-    void Absorption()
+void Absorption()
+{
+    if (Input.GetKeyDown(KeyCode.E))
     {
-        if (Input.GetKeyDown(KeyCode.E))
+        print("pressed");
+        // 만약 플레이어가 스폰지 안에 들어가 있다면
+        if (isPlayerInSponge)
         {
-            print("pressed");
-            // 만약 플레이어가 스폰지 안에 들어가 있다면
-            if (isPlayerInSponge)
+            if (shotDirection == Direction.East)
             {
-                if (shotDirection == Direction.East)
-                {
-                    print("발사!히히");
-                    player.SetActive(true);
-                    player.transform.position = transform.position;
-                    print("asdf" + shootingSpeed);
-                    playerRigidbody2D.AddForce(Vector2.right * shootingSpeed, ForceMode2D.Impulse);
-                    color.color = new Color(255f / 255f, 242f / 255f, 114f / 255f);
-                    isPlayerInSponge = false; // 발사 후 스폰지를 빠져나왔음을 표시
-                }
-
-                else if (shotDirection == Direction.West)
-                {
-                    print("발사!히히");
-                    player.SetActive(true);
-                    player.transform.position = transform.position;
-                    print("asdf" + shootingSpeed);
-                    player.transform.position += Vector3.left * shootingSpeed;
-                    color.color = new Color(255f / 255f, 242f / 255f, 114f / 255f);
-                    isPlayerInSponge = false; // 발사 후 스폰지를 빠져나왔음을 표시
-                }
-                else if (shotDirection == Direction.North)
-                {
-                    print("hhhhhhh 발사!히히");
-                    player.SetActive(true);
-                    player.transform.position = transform.position;
-                    print("asdf" + shootingSpeed);
-                    player.transform.position += Vector3.up * shootingSpeed;
-                    color.color = new Color(255f / 255f, 242f / 255f, 114f / 255f);
-                    isPlayerInSponge = false; // 발사 후 스폰지를 빠져나왔음을 표시
-                }
-                else if (shotDirection == Direction.South)
-                {
-                    print("발사!히히");
-                    player.SetActive(true);
-                    player.transform.position = transform.position;
-                    print("asdf" + shootingSpeed);
-                    player.transform.position += Vector3.down * shootingSpeed;
-                    color.color = new Color(255f / 255f, 242f / 255f, 114f / 255f);
-                    isPlayerInSponge = false; // 발사 후 스폰지를 빠져나왔음을 표시
-                }
-
+                print("발사!히히");
+                player.SetActive(true);
+                player.transform.position = transform.position;
+                print("asdf" + shootingSpeed);
+                player.transform.position += Vector3.right * shootingSpeed;
+                color.color = new Color(255f / 255f, 242f / 255f, 114f / 255f);
+                isPlayerInSponge = false; // 발사 후 스폰지를 빠져나왔음을 표시
             }
-            else if (isTouchedPlayer)
+
+            else if (shotDirection == Direction.West)
             {
+                print("발사!히히");
+                player.SetActive(true);
+                player.transform.position = transform.position;
+                print("asdf" + shootingSpeed);
+                player.transform.position += Vector3.left * shootingSpeed;
+                color.color = new Color(255f / 255f, 242f / 255f, 114f / 255f);
+                isPlayerInSponge = false; // 발사 후 스폰지를 빠져나왔음을 표시
+            }
+            else if (shotDirection == Direction.North)
+            {
+                print("hhhhhhh 발사!히히");
+                player.SetActive(true);
+                player.transform.position = transform.position;
+                print("asdf" + shootingSpeed);
+                player.transform.position += Vector3.up * shootingSpeed;
+                color.color = new Color(255f / 255f, 242f / 255f, 114f / 255f);
+                isPlayerInSponge = false; // 발사 후 스폰지를 빠져나왔음을 표시
+            }
+            else if (shotDirection == Direction.South)
+            {
+                print("발사!히히");
+                player.SetActive(true);
+                player.transform.position = transform.position;
+                print("asdf" + shootingSpeed);
+                player.transform.position += Vector3.down * shootingSpeed;
+                color.color = new Color(255f / 255f, 242f / 255f, 114f / 255f);
+                isPlayerInSponge = false; // 발사 후 스폰지를 빠져나왔음을 표시
+            }
+               
+        }
+        else if (isTouchedPlayer)
+        { 
                 player.SetActive(false);
-                color.color = new Color(114f / 255f, 235f / 255f, 255f / 255f);
-                isPlayerInSponge = true; // 플레이어를 스폰지 안에 넣음
-
-            }
+                color.color = new Color(114f/255f,235f/255f,255f/255f);
+            isPlayerInSponge = true; // 플레이어를 스폰지 안에 넣음
+            
         }
     }
+}
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Player"))
+        if(collision.CompareTag("Player"))
         {
             isTouchedPlayer = true;
         }
@@ -121,3 +118,4 @@ public class SpongeController : MonoBehaviour
 
 
 }
+
